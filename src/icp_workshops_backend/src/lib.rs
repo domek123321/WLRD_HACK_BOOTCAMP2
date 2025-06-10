2421770956ed28e21005c1b09b2e60c3c86f43b2
Dominik Ziolkowski <Stachupompka@onet.pl>
2025-06-10 17:39:17 +0000

    ICP ai

--- src/icp_workshops_backend/src/lib.rs
+++ src/icp_workshops_backend/src/lib.rs
@@ -1,4 +1,43 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use ic_cdk::api::management_canister::http_request::HttpHeader;
+use ic_cdk::api::management_canister::http_request::http_request;
+use ic_cdk::api::management_canister::http_request::CanisterHttpRequestArgument;
+use ic_cdk::api::management_canister::http_request::HttpMethod;
+
+#[ic_cdk::update]
+async fn translate(text: String) -> String {
+    let token = "";
+    
+// pub struct CanisterHttpRequestArgument {
+//     pub url: String,
+//     pub max_response_bytes: Option<u64>,
+//     pub method: HttpMethod,
+//     pub headers: Vec<HttpHeader>,
+//     pub body: Option<Vec<u8>>,
+//     pub transform: Option<TransformContext>,
+// }
+   let (res,) = http_request(
+        CanisterHttpRequestArgument {
+            url: "https://api-inference.huggingface.co/models/google-t5/t5-base".to_string(),
+            max_response_bytes: None,
+            method: HttpMethod::POST,
+            headers: vec![
+                      HttpHeader {
+                    name: "Authorization".to_string(),
+                    value: format!("Bearer {}", token),
+                },
+                             HttpHeader {
+                    name: "Content-Type".to_string(),
+                    value: "application/json".to_string(),
+                }
+            ],
+            body: Some(format!(r#"{{"inputs": "{}"}}"#, text).into()),
+            transform: None,
+        }, (20_849_972_800u128 + text.len() as u128 * 5_200u128)
+    ).await.unwrap();
+
+    
+
+    let body = String::from_utf8(res.body).unwrap();
+   ic_cdk::println!("{:?}", body);
+    format!("Hello, {}!", text)
 }
